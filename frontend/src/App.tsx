f9a0ff28d79c28805377a5d652d145fc06c1d700
Kaua Silva <kauaalexandredasilva62@gmail.com>
2022-09-06 12:07:48 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificaationButton from "./assets/components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Olá mundo!</h1>
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificaationButton />
+      <NotificaationButton />
+    </>
   )
 }
 
